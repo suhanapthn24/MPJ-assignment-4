54e456f0394720ab6e4b9ff3d992dd363b87c37a
suhanapthn24 <suhanapthn24@gmail.com>
2026-04-06 10:27:46 +0530

    Customer database

--- BankingSystemTxt.java
+++ BankingSystemTxt.java
@@ -2,31 +2,31 @@ import java.io.*;
 import java.util.*;
 
 // Custom Exceptions
-class MinimumBalanceException extends Exception {
-    MinimumBalanceException(String msg) {
+class MinBalanceException extends Exception {
+    MinBalanceException(String msg) {
         super(msg);
     }
 }
 
-class InvalidCIDException extends Exception {
-    InvalidCIDException(String msg) {
+class CIDException extends Exception {
+    CIDException(String msg) {
         super(msg);
     }
 }
 
 class Customer {
     int cid;
-    String cname;
-    double amount;
+    String name;
+    double amt;
 
-    Customer(int cid, String cname, double amount) {
+    Customer(int cid, String name, double amt) {
         this.cid = cid;
-        this.cname = cname;
-        this.amount = amount;
+        this.name = name;
+        this.amt = amt;
     }
 
     public String toString() {
-        return cid + "," + cname + "," + amount;
+        return cid + "," + name + "," + amt;
     }
 }
 
@@ -34,38 +34,38 @@ public class BankingSystemTxt {
     static final String FILE_NAME = "customers.txt";
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner s = new Scanner(System.in);
         int choice;
 
         do {
-            System.out.println("\n--- Banking Menu ---");
-            System.out.println("1. Create Account");
+            System.out.println("\n Menu: ");
+            System.out.println("1. Create");
             System.out.println("2. Deposit");
             System.out.println("3. Withdraw");
-            System.out.println("4. Display All");
+            System.out.println("4. List all");
             System.out.println("5. Exit");
             System.out.print("Enter choice: ");
-            choice = sc.nextInt();
+            choice = s.nextInt();
 
             try {
                 switch (choice) {
                     case 1:
-                        createAccount(sc);
+                        createAccount(s);
                         break;
                     case 2:
-                        deposit(sc);
+                        deposit(s);
                         break;
                     case 3:
-                        withdraw(sc);
+                        withdraw(s);
                         break;
                     case 4:
                         displayAll();
                         break;
                     case 5:
-                        System.out.println("Exiting...");
+                        System.out.println("Exit");
                         break;
                     default:
-                        System.out.println("Invalid choice!");
+                        System.out.println("Invalid choice");
                 }
             } catch (Exception e) {
                 System.out.println("Error: " + e.getMessage());
@@ -73,16 +73,16 @@ public class BankingSystemTxt {
 
         } while (choice != 5);
 
-        sc.close();
+        s.close();
     }
 
     // Create Account
-    static void createAccount(Scanner sc) throws Exception {
+    static void createAccount(Scanner s) throws Exception {
         System.out.print("Enter CID (1-20): ");
-        int cid = sc.nextInt();
+        int cid = s.nextInt();
 
         if (cid < 1 || cid > 20)
-            throw new InvalidCIDException("CID must be between 1 and 20!");
+            throw new CIDException("CID must be between 1 and 20!");
 
         List<Customer> list = readAll();
         for (Customer c : list) {
@@ -91,18 +91,18 @@ public class BankingSystemTxt {
             }
         }
 
-        sc.nextLine();
+        s.nextLine();
         System.out.print("Enter Name: ");
-        String name = sc.nextLine();
+        String name = s.nextLine();
 
-        System.out.print("Enter Amount: ");
-        double amt = sc.nextDouble();
+        System.out.print("Enter amt: ");
+        double amt = s.nextDouble();
 
         if (amt <= 0)
-            throw new ArithmeticException("Amount must be positive!");
+            throw new ArithmeticException("amt must be positive!");
 
         if (amt < 1000)
-            throw new MinimumBalanceException("Minimum balance is Rs.1000!");
+            throw new MinBalanceException("Minimum balance is Rs.1000");
 
         Customer c = new Customer(cid, name, amt);
 
@@ -111,16 +111,16 @@ public class BankingSystemTxt {
         bw.newLine();
         bw.close();
 
-        System.out.println("Account created successfully!");
+        System.out.println("Successful");
     }
 
     // Deposit
-    static void deposit(Scanner sc) throws Exception {
+    static void deposit(Scanner s) throws Exception {
         System.out.print("Enter CID: ");
-        int cid = sc.nextInt();
+        int cid = s.nextInt();
 
-        System.out.print("Enter deposit amount: ");
-        double amt = sc.nextDouble();
+        System.out.print("Enter deposit amt: ");
+        double amt = s.nextDouble();
 
         if (amt <= 0)
             throw new ArithmeticException("Amount must be positive!");
@@ -130,7 +130,7 @@ public class BankingSystemTxt {
         boolean found = false;
         for (Customer c : list) {
             if (c.cid == cid) {
-                c.amount += amt;
+                c.amt += amt;
                 found = true;
             }
         }
@@ -145,12 +145,12 @@ public class BankingSystemTxt {
     }
 
     // Withdraw
-    static void withdraw(Scanner sc) throws Exception {
+    static void withdraw(Scanner s) throws Exception {
         System.out.print("Enter CID: ");
-        int cid = sc.nextInt();
+        int cid = s.nextInt();
 
-        System.out.print("Enter withdrawal amount: ");
-        double amt = sc.nextDouble();
+        System.out.print("Enter withdrawal amt: ");
+        double amt = s.nextDouble();
 
         List<Customer> list = readAll();
 
@@ -158,10 +158,10 @@ public class BankingSystemTxt {
         for (Customer c : list) {
             if (c.cid == cid) {
 
-                if (amt > c.amount)
+                if (amt > c.amt)
                     throw new ArithmeticException("Insufficient balance!");
 
-                c.amount -= amt;
+                c.amt -= amt;
                 found = true;
             }
         }
@@ -180,7 +180,7 @@ public class BankingSystemTxt {
         BufferedReader br = new BufferedReader(new FileReader(FILE_NAME));
         String line;
 
-        System.out.println("\nCID | Name | Amount");
+        System.out.println("\nCID | Name | amt");
 
         while ((line = br.readLine()) != null) {
             String[] data = line.split(",");
